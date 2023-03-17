7da9e6bfff1256336fad719c42fc12ad049f6a38
jsonwamuyu <jysonmuchiri@gmail.com>
2023-03-17 13:54:43 -0700

    aphabets

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Return: Always 0 (Success)
+ */
+
+
+int main(void)
+{
+	putchar("abcdefghijklmnopqrstuvwxyz\n");
+	return (0);
+}
