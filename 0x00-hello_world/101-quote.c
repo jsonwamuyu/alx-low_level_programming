c0f7f542ee6f39796048dd8c62da4fb1d279ceb1
jsonwamuyu <jysonmuchiri@gmail.com>
2023-03-16 17:53:09 -0700

    Quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - entry point
+ *
+ *
+ * Return: Always 1
+ */
+
+int main(void)
+{
+	fwrite("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	return (1);
+}
