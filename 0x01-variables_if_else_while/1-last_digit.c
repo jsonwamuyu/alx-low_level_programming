25e17ae8a702783ccbae91f62c9e088ef0195082
jsonwamuyu <jysonmuchiri@gmail.com>
2023-03-17 13:30:00 -0700

    Fix typo

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -22,7 +22,7 @@ int main(void)
 		printf("Last digit of %d is %d and is greater than 5\n", n , k);
 	}
 	if (k == 0){
-		printf("Last digit of %d is %d and is zero\n", n, k);
+		printf("Last digit of %d is %d and is 0\n", n, k);
 	
 	}
 	if (k < 6 && k != 0)
