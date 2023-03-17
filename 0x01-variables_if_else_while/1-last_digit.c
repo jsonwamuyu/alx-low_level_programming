8d7ffae29a0b764a9118c0b871b329e93322fb3f
jsonwamuyu <jysonmuchiri@gmail.com>
2023-03-17 13:39:05 -0700

    Remove space

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -19,9 +19,10 @@ int main(void)
 
 	if (k > 5)
 	{
-		printf("Last digit of %d is %d and is greater than 5\n", n , k);
+		printf("Last digit of %d is %d and is greater than 5\n", n, k);
 	}
-	if (k == 0){
+	if (k == 0)
+	{
 		printf("Last digit of %d is %d and is 0\n", n, k);
 	
 	}
