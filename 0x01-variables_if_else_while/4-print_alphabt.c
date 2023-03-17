e45d1172d322190d1f36d33f335a767c9f5e85ed
jsonwamuyu <jysonmuchiri@gmail.com>
2023-03-17 15:25:36 -0700

    Remove q and e

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char a;
+
+	a = 'a';
+
+	while(a <= 'z')
+	{
+		if (a != 'e' || a != 'q')
+		{
+			putchar(a);
+		}
+		a++;
+	}
+	putchar('\n');
+	return (0);
+}
