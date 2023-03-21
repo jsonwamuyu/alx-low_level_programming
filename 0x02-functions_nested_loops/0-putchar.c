24e34c0833bf14d496f5b01f234cafe772ebfd01
jsonwamuyu <jysonmuchiri@gmail.com>
2023-03-21 16:50:05 -0700

    Pustchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,7 +1,21 @@
-#include <stdio.h>
+#include "main.h"
 
-int main()
-{	
-	printf("_putchar\n");
-	return (0);
+/**
+ * main - Program that prints _putchar
+ * followed by a new line
+ * Return: 0 (Success)
+ */
+
+int main(void)
 {
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
+}
