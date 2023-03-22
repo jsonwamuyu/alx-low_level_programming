4fc008440e62b3d550fee2f4081f2e3af88435e9
jsonwamuyu <jysonmuchiri@gmail.com>
2023-03-21 17:24:41 -0700

    alpabet 10 times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints 10 times the alphabet,
+ *  in lowercase, followed by a new line
+ */
+void print_alphabet_x10(void)
+{
+	char alpha;
+	int i;
+
+	i = 0;
+
+	while (i < 10)
+	{
+		alpha = 'a';
+		while (alpha <= 'z')
+		{
+			_putchar(alpha);
+			alpha++;
+		}
+		_putchar('\n');
+		i++;
+	}
+}
