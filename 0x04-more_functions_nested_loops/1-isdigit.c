6d8608eb89343d1160ad1ded776d615206c23fcb
jsonwamuyu <jysonmuchiri@gmail.com>
2023-03-23 13:41:46 -0700

    Fix indentation

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -14,9 +14,12 @@ int _isdigit(int c);
 
 int _isdigit(int c)
 {
-  if (c >= '0' && c <= '9')
-    return (1);
-  else
-    return (0);
-
+	if (c >= '0' && c <= '9')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
 }
