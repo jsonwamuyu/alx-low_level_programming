1c241bb467f34b8495bc0ae12a10822106bc9be4
jsonwamuyu <jysonmuchiri@gmail.com>
2023-03-23 14:06:57 -0700

    Document

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,11 +1,19 @@
 #include "main.h"
 
 
+/**
+ *mul - function declaration
+ *Description - find product of two numbers
+ *@a - first number
+ *@b - second number
+ *Return - product of a and b
+ */
+
 
 int mul(int a, int b);
 
 
-/*
+/**
  *mul - function that multiply two numbers"
  *@a - first number
  *@b - second number
@@ -14,6 +22,8 @@ int mul(int a, int b);
 
 int mul(int a, int b)
 {
-  return (a * b);
+  int product;
+  product = a * b;
+  return (product);
 
 }
