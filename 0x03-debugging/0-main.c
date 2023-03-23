dcd8b5aff4fa113308ad663350089c681a7f6199
jsonwamuyu <jysonmuchiri@gmail.com>
2023-03-22 18:32:32 -0700

    Fix logic

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,10 +1,17 @@
-#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Test function for positive or negative
+ * Return: 0
+ */
+
 
 int main(void)
 {
   int r;
 
-  r = positive_or_negative(0);
+  r = 0;
+  positive_or_negative(0);
   
   return (0);
 }
--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,6 +1,8 @@
 #ifndef MAIN_H
 #def MAIN_H
 
+#include <stdio.h>
+
 
 void positive_or_negative(int);
 
