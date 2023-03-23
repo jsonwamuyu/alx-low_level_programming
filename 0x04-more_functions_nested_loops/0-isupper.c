16399cd97236d6612ac611436cc7d6d1d242cf48
jsonwamuyu <jysonmuchiri@gmail.com>
2023-03-23 13:03:18 -0700

    Check lower/uppercase

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/*
+ *_isupper - function that checks a character is uppercase
+ *@c - holds value of the character to be checked 
+ */
+
+int _isupper(int c)
+{
+  ((c >= "A") && (C <= "Z"));
+}
