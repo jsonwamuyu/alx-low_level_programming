fc04fc69950cf8f0695b4c9dd592fb58ce11d18d
jsonwamuyu <jysonmuchiri@gmail.com>
2023-03-27 19:39:45 -0700

    Fix typo

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -5,15 +5,25 @@
  * @str: input
  * Return: print
  */
-
 void puts2(char *str)
 {
-	int i;
+	int longi = 0;
+	int t = 0;
+	char *y = str;
+	int o;
 
-	for (i = 0; str[i] != '\0'; i++)
+	while (*y != '\0')
 	{
-		_putchar(str[i]);
-		i++;
+		y++;
+		longi++;
+	}
+	t = longi - 1;
+	for (o = 0 ; o <= t ; o++)
+	{
+		if (o % 2 == 0)
+	{
+		_putchar(str[o]);
+	}
 	}
 	_putchar('\n');
 }
